9b87e16ceb444fcd02e32b02023040dea90e68a7
Stolbov Ilya <kretov.il@mail.ru>
2020-11-25 19:25:13 +0300

    Обновил поиск; сделал страницу пользователя

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,31 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Search = () => {
-  const {show} = useContext(AlertContext);
+  const [value, setValue] = useState('');
+  const alert = useContext(AlertContext);
+  const github = useContext(GithubContext);
 
   const onSubmit = e => {
-    if (e.key === 'Enter') {
-      show('Pressed Enter');
+    if (e.key !== 'Enter') {
+      return;
+    }
+
+    github.clearUsers();
+
+    if (value.trim()) {
+      alert.hide();
+      github.search(value.trim());
+    } else {
+      alert.show('Enter username');
     }
   };
 
   return (
     <div className="form-group">
-      <input type="text" className="form-control" placeholder="Search..." onKeyPress={onSubmit}/>
+      <input type="text" className="form-control" placeholder="Search..." value={value}
+        onKeyPress={onSubmit} onChange={e => setValue(e.target.value)}/>
     </div>
   )
 }
